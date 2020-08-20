6f59f1e0dbc0ace5abfbc2cd90964995b3fc6b4a
Naren404 <narendrasunar47@gmail.com>
2020-08-20 10:33:44 +0000

    Fixed bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates FILL_IN, presence:true
-  validates FILL_IN, presence:true
+  validates content, presence:true
+  validates content, presence:true
 end
